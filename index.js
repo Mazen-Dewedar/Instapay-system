b8bb7b1f92bb9350b3eb5f8390b6268ea99c80e0
Mazen-Dewedar <mazenhesham172@gmail.com>
2025-09-28 15:13:55 +0300

    last

---
+++ index.js
@@ -0,0 +1,56 @@
+let balance = 0;
+let input = document.querySelector('input')
+let value = document.querySelector('span')
+let transactions = []
+let table = document.querySelector('table tbody')
+
+let showAmount = () => {
+    value.innerText = +balance;
+}
+showAmount();
+let deposite = () => {
+    let amount = +input.value;
+    transactions.push({
+        balanceBefore: balance,
+        Amount: amount,
+        Type: 'Deposite',
+        balanceAfter: balance + amount
+    })
+    balance = balance + amount;
+    showAmount();
+    input.value = "";
+
+}
+let withdraw = () => {
+    let amount = +input.value;
+    if (balance >= amount) {
+        transactions.push({
+            balanceBefore: balance,
+            Amount: amount,
+            Type: 'Withdraw',
+            balanceAfter: balance - amount
+        })
+        balance = balance - amount;
+        showAmount();
+        input.value = "";
+    } else {
+        alert("You don't have enugh money ")
+        input.value = "";
+    }
+}
+
+let showTransactions = () => {
+    table.innerHTML=''
+    transactions.forEach((el, index) => {
+        let row = `
+        <tr>
+            <td>${index + 1}</td>
+            <td>${el.balanceBefore}</td>
+            <td>${el.Amount}</td>
+            <td>${el.Type}</td>
+            <td>${el.balanceAfter}</td>
+        </tr>`
+
+        table.innerHTML += row
+    })
+}
